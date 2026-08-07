0ff8b39f46ee4334a570ca03e964a0cbacaa200c
Songyun Liu <270969297@yoobeestudent.ac.nz>
2026-08-07 14:25:44 +0800

    Update Activity4.py

--- Activity4.py
+++ Activity4.py
@@ -11,9 +11,9 @@ def main():
     # Determine BMI category
     if bmi < 18.5:
         print("You are underweight.")
-    elif 18.5 <= bmi < 24.9:
+    elif 18.5 <= bmi < 25:
         print("You have a normal weight.")
-    elif 25 <= bmi < 29.9:
+    elif 25 <= bmi < 30:
         print("You are overweight.")
     else:
         print("You are obese.")
