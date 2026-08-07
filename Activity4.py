57bf039f277bcfb15667db1ff993d483dfe78a5c
Songyun Liu <270969297@yoobeestudent.ac.nz>
2026-08-07 14:23:59 +0800

    Complete Week 1

---
+++ Activity4.py
@@ -0,0 +1,20 @@
+def main():
+    #Get the user's weight in kilograms
+    weight = float(input("Enter your weight (kg): "))
+    #Get the user's height in metres
+    height = float(input("Enter your height (m): "))
+    #Calculate BMI，BMI = weight ÷ height²
+    bmi = weight / (height * height)
+    # Display the BMI rounded to 2 decimal places
+    print("Your BMI is:", round(bmi, 2))
+
+    # Determine BMI category
+    if bmi < 18.5:
+        print("You are underweight.")
+    elif 18.5 <= bmi < 24.9:
+        print("You have a normal weight.")
+    elif 25 <= bmi < 29.9:
+        print("You are overweight.")
+    else:
+        print("You are obese.")
+main()
